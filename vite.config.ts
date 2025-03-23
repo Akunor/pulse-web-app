b30a8a39e8bf1e64b86eac3fb7af84964b164303
Akunor <samuel.stoddard@gmail.com>
2025-03-23 17:44:29 +0000

    Build error fix 3

--- vite.config.ts
+++ vite.config.ts
@@ -7,10 +7,13 @@ export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
+    include: ['next/server', '@supabase/auth-helpers-nextjs']
   },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
+      'next/server': 'next/dist/server',
+      'next': 'next/dist'
     },
   },
 });
