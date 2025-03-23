c1ff8d5d1beeda218c5aa263a2fd060e8b51a18b
Akunor <samuel.stoddard@gmail.com>
2025-03-23 17:25:23 +0000

    Env variables fix 2

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -1,9 +1,9 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
+  readonly SUPABASE_URL: string;
+  readonly SUPABASE_SERVICE_ROLE_KEY: string;
   readonly VITE_APP_TITLE: string;
-  readonly VITE_SUPABASE_URL: string;
-  readonly VITE_SUPABASE_ANON_KEY: string;
   readonly VITE_WEBAPP_URL: string;
 }
 
